33c304defb1bcb0893812c5b921e06dd5bd11351
Saoirse Byrne <saoirsebyrne98@gmail.com>
2024-05-25 15:59:22 +0930

    Progessing through to step 5

--- tic-tac-toe-lab/js/app.js
+++ tic-tac-toe-lab/js/app.js
@@ -1,27 +1,54 @@
-/*-------------------------------- Constants --------------------------------*/
-// 5) DEFINE THE REQUIRED CONSTANTS
 
 
-/*---------------------------- Variables (state) ----------------------------*/
-// 1) DEFINE THE VARIABLES I NEED TO TRACK THE STATE OF THE GAME
+let board;
+let turn;
+let winner;
+let tie;
 
-const board 
+const squareElements = document.querySelectorAll('.sqr');
 
-
-/*------------------------ Cached Element References ------------------------*/
-// 2) STORE THESE ELEMENTS IN HERE
-
-
-/*-------------------------------- Functions --------------------------------*/
-// 3) UPON LOADING, THE GAME STATE SHOULD BE INITIALISED
-//      A FUNCTION SHOULD BE CALLED TO RENGER THIS GAME STATE
-// 4) THE STATE OF THE GAME SHOULD BE RENDERED TO THE USER
-
-// 6) HANDLE A PLAYER CLICK A SQUARE WITH A 'HANDLECLICK' FUNCTION
+const messageElement = document.querySelector('#message');
 
 
-/*----------------------------- Event Listeners -----------------------------*/
+function init() { 
+    board = ['', '', '', '', '', '', '', '', ''];
+    turn = 'X';
+    winner = false;
+    tie = false;
+    render();
+}
 
+window.onload = init();
 
+function render() {
+    updateBoard();
+    updateMessage();
+};
 
+function updateBoard() {
+    const squares = document.querySelectorAll('.sqr');
+    board.forEach((cell, index) => {
+        squares[index].textContent = cell;
+    });
+}
 
+function updateMessage() {
+    if (winner === false && tie === false) {
+        messageElement.textContent = `Its ${turn}'s turn`;
+    } else if (winner === false && tie === true) {
+        messageElement.textContent = "It's a tie!";
+    } else {
+        messageElement.textContent = `lallala`;
+    }
+}
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
