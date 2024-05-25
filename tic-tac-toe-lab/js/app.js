c5e0fcda546625229cb8584c015864499b70516b
Saoirse Byrne <saoirsebyrne98@gmail.com>
2024-05-25 10:57:16 +0930

    Set Up Complete, beginning the steps

---
+++ tic-tac-toe-lab/js/app.js
@@ -0,0 +1,27 @@
+/*-------------------------------- Constants --------------------------------*/
+// 5) DEFINE THE REQUIRED CONSTANTS
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+// 1) DEFINE THE VARIABLES I NEED TO TRACK THE STATE OF THE GAME
+
+const board 
+
+
+/*------------------------ Cached Element References ------------------------*/
+// 2) STORE THESE ELEMENTS IN HERE
+
+
+/*-------------------------------- Functions --------------------------------*/
+// 3) UPON LOADING, THE GAME STATE SHOULD BE INITIALISED
+//      A FUNCTION SHOULD BE CALLED TO RENGER THIS GAME STATE
+// 4) THE STATE OF THE GAME SHOULD BE RENDERED TO THE USER
+
+// 6) HANDLE A PLAYER CLICK A SQUARE WITH A 'HANDLECLICK' FUNCTION
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
